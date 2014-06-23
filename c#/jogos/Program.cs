0ced379ed63419258947370b0d9b57e5ae89e813
Andrew Fugier <fugier@gmail.com>
2014-06-23 13:27:00 -0700

    fix extra newline on games that are not completed

--- c#/jogos/Program.cs
+++ c#/jogos/Program.cs
@@ -2,4 +2,4 @@
 class Program{
     static void Main(string[] args){
         foreach (var e in System.Web.Helpers.Json.Decode((string)(new System.Net.WebClient()).DownloadString("http://worldcup.sfg.io/matches")))
-            Console.WriteLine((e.status == "completed") ? " {0} {1} vs {2} {3}":"", e.home_team.country, e.home_team.goals, e.away_team.country, e.away_team.goals); } }
+            Console.Write((e.status == "completed") ? " {0} {1} vs {2} {3}\n":"", e.home_team.country, e.home_team.goals, e.away_team.country, e.away_team.goals); } }
