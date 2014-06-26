8b162a868dad1493fd8fef2c9fb70e0890a0d4bb
Phillip Nordwall <Phillip.Nordwall+git@gmail.com>
2014-06-25 19:34:20 -0700

    Adding racket files for jogos.

--- racket/jogos.rkt
+++ racket/jogos.rkt
@@ -1,12 +1,22 @@
 #lang racket
 (require net/http-client json)
 
-(map (lambda (y)
-       (list (hash-ref (hash-ref y 'home_team) 'country)
-             (hash-ref (hash-ref y 'home_team) 'goals)
-             (hash-ref (hash-ref y 'away_team) 'country)
-             (hash-ref (hash-ref y 'away_team) 'goals)
-             ))
+;query page
+;parse json
+;filter that the status is completed
+;for each match grab the country and goals entries from the team
+; separate home and away with a 'vs'
+; format entries as strings
+; separate them with spaces
+;separate matches with newlines
+;display the results
+(display (string-join (map (lambda (match)
+       (string-join (map ~a (list (hash-ref (hash-ref match 'home_team) 'country)
+             (hash-ref (hash-ref match 'home_team) 'goals)
+             "vs"
+             (hash-ref (hash-ref match 'away_team) 'country)
+             (hash-ref (hash-ref match 'away_team) 'goals)
+             )) " "))
      (let-values ([(stat head response) (http-sendrecv "worldcup.sfg.io" "/matches")])
-       (filter (lambda (x) (equal? "completed" (hash-ref x 'status)))
-               (read-json response))))
+       (filter (lambda (match) (equal? "completed" (hash-ref match 'status)))
+               (read-json response)))) "\n"))
